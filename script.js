ade71466d680b7b8471c8b5658d6053b094670b0
sqnder666 <i.irudovi21@gmail.com>
2023-02-07 19:50:20 +0300

    10

--- script.js
+++ script.js
@@ -3,14 +3,8 @@ let num;
 num = 123;
 alert(num); 
 
-let a, b, c; // объявляем все 3 переменные
+let a, b, c; 
 
-a = 2 * 2 + 2;
-alert(a);
-b = 3 * (2 + 6);
-alert(b);
-
-
-c = (2 * 5) / 4;
-
-alert(c);
+a = 2.7;
+b = 1.3;
+alert(a - b);
