f612c3e129a668c7fefc94ca43baef822b2a37ab
sqnder666 <i.irudovi21@gmail.com>
2023-02-07 19:57:00 +0300

    15

--- script.js
+++ script.js
@@ -1,3 +1,19 @@
 "use strict";
-let str = 'Ilya Rudov';
-alert(str); 
+// exr1
+let str;
+str = '!!!'
+alert(str);
+
+// exr2
+let a;
+let b;
+a = 'java';
+b = 'script';
+alert(a + b);
+
+// exr3
+let c;
+let d;
+c = 'hello';
+d = 'world';
+alert(c + d);
