edd624759b863c58056838c82322581dc0cba24d
sqnder666 <i.irudovi21@gmail.com>
2023-02-07 19:58:16 +0300

    16

--- script.js
+++ script.js
@@ -1,19 +1,3 @@
 "use strict";
-// exr1
-let str;
-str = '!!!'
-alert(str);
-
-// exr2
-let a;
-let b;
-a = 'java';
-b = 'script';
-alert(a + b);
-
-// exr3
-let c;
-let d;
-c = 'hello';
-d = 'world';
-alert(c + d);
+let str = 'dfdsdsfdsfdsfdfssdfdfsdfs';
+alert(str.length);
