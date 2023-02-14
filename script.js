b2662aca95a159cf354763afbe565210db55b4d8
sqnder666 <i.irudovi21@gmail.com>
2023-02-14 23:03:41 +0300

    51

--- script.js
+++ script.js
@@ -1,4 +1,9 @@
 "use strict";
 
-let user = { name: "Ilya", surname: "Rudov", patronymic: "Sergeevich" };
-document.write(`${user.name} ${user.surname} ${user.patronymic}`);
+let today = new Date();
+var dd = String(today.getDate()).padStart(2, "0");
+var mm = String(today.getMonth() + 1).padStart(2, "0");
+var yyyy = today.getFullYear();
+console.log(yyyy);
+let date = { year: yyyy, month: mm, day: dd };
+document.write(`${date.year}-${date.month}-${date.day}`);
