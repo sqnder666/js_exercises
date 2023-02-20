06ba37b8185a0df6c235edfad968793460768e6f
sqnder666 <i.irudovi21@gmail.com>
2023-02-20 22:52:43 +0300

    62

--- script.js
+++ script.js
@@ -1,23 +1,6 @@
 "use strict";
 
-let key = "x";
+let obj = { x: 1, y: 2, z: 3 };
 
-let obj = {
-  [key]: 1,
-  y: 2,
-  z: 3,
-};
-
-console.log(obj.x);
-
-let key1 = "x";
-let key2 = "y";
-let key3 = "z";
-
-let obj2 = {
-  x: [key1],
-  y: [key2],
-  z: [key3],
-};
-
-console.log(obj.y);
+console.log("x" in obj);
+console.log("w" in obj);
