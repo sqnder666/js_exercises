d26adc9d10188c4aa1581f018dc44f53b46a8d53
sqnder666 <i.irudovi21@gmail.com>
2023-02-20 23:20:13 +0300

    81

--- script.js
+++ script.js
@@ -1,29 +1,16 @@
 "use strict";
 
-let test1 = "3";
-let test2 = "3";
-if (test1 == test2) {
-  console.log("+++");
-} else {
-  console.log("---");
+let num = Math.floor(Math.random() * 21);
+console.log(`num = ${num}`);
+if (num > 0 && num < 5) {
+  console.log(`0 < num < 5`);
+} else if (num > 10 && num <= 20) {
+  console.log(`10 < num < 20`);
 }
-if (test1 === test2) {
-  console.log("+++");
-} else {
-  console.log("---");
-}
-if (test1 == test2) {
-  console.log("+++");
-} else {
-  console.log("---");
-}
-if (test1 === test2) {
-  console.log("+++");
-} else {
-  console.log("---");
-}
-if (test1 === test2) {
-  console.log("+++");
-} else {
-  console.log("---");
+let num1 = Math.floor(Math.random() * 10);
+let num2 = Math.floor(Math.random() * 10);
+console.log(`num1 = ${num1}`);
+console.log(`num2 = ${num2}`);
+if (num1 <= 1 && num2 >= 3) {
+  console.log(`num1 <= 1 && num2 >= 3`);
 }
