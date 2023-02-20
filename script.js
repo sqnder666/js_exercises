d69e324304c07a33b0341e82b65d3ad98e5af581
sqnder666 <i.irudovi21@gmail.com>
2023-02-20 23:10:31 +0300

    71

--- script.js
+++ script.js
@@ -1,6 +1,9 @@
 "use strict";
 
-const arr = [1, 2, 3, 4, 5];
-const res = arr[1] + arr[2];
-
-console.log(res);
+let obj = { x: 1, y: 2, z: 3 };
+console.log(obj.x);
+let key = "x";
+console.log(obj[key]);
+let sum = obj.x + obj.y + obj.x;
+console.log(sum);
+console.log(Object.keys(obj).length);
