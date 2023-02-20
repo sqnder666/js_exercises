d51699b050de8bf16fb2d2248952d47faf1712ae
sqnder666 <i.irudovi21@gmail.com>
2023-02-20 23:21:26 +0300

    83

--- script.js
+++ script.js
@@ -1,62 +1,7 @@
 "use strict";
 
-let num1 = -10;
-let num2 = -10;
-
-if (num1 >= 0 || num2 >= 0) {
-  console.log("+++");
-} else {
-  console.log("---");
-}
-num1 = 0;
-num2 = 0;
-
-if (num1 >= 0 || num2 >= 0) {
-  console.log("+++");
-} else {
-  console.log("---");
-}
-num1 = 0;
-num2 = 5;
-
-if (num1 >= 0 || num2 >= 0) {
-  console.log("+++");
-} else {
-  console.log("---");
-}
-num1 = 5;
-num2 = 5;
-
-if (num1 >= 0 || num2 >= 0) {
-  console.log("+++");
-} else {
-  console.log("---");
-}
-num1 = -5;
-num2 = 15;
-
-if (num1 >= 0 || num2 >= 0) {
-  console.log("+++");
-} else {
-  console.log("---");
-}
-let num = 1;
-
-if (num == 0 || num == 1) {
-  console.log("+++");
-} else {
-  console.log("---");
-}
-num = 2;
-
-if (num == 0 || num == 1) {
-  console.log("+++");
-} else {
-  console.log("---");
-}
-num = 2;
-
-if (num == 0 || num == 1 || num == 2) {
+if (num == 0 || (num > 1 && num < 5)) {
+  // порядок: 1 - num == 0, 2 - num > 1 И num < 5
   console.log("+++");
 } else {
   console.log("---");
