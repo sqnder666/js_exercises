a91989ecd6c1b2f20f04eda73e9d27c3be999fcf
sqnder666 <i.irudovi21@gmail.com>
2023-02-20 23:04:52 +0300

    65

--- script.js
+++ script.js
@@ -1,7 +1,9 @@
 "use strict";
 
 console.log(typeof { x: 1, y: 2, z: 3 });
-console.log(typeof {});
-let obj = { x: 1, y: 2, z: 3 };
-console.log(typeof obj);
-console.log(typeof obj["x"]);
+console.log(typeof [1, 2, 3]);
+let arr = [1, 2, 3];
+console.log(typeof arr);
+console.log(typeof arr[0]);
+let arr_2 = ["1", "2", "3"];
+console.log(typeof arr_2[0]);
