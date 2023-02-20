fc86e23ab7d4b3be1c95f65f509b4237f9921b37
sqnder666 <i.irudovi21@gmail.com>
2023-02-20 23:07:53 +0300

    69

--- script.js
+++ script.js
@@ -1,9 +1,9 @@
 "use strict";
 
-let arr1 = [1, 2, 3];
-let arr2 = arr1;
-arr1[0] = "a";
-console.log(arr2);  
-arr2[1] = "b";
-console.log(arr1);
-console.log(arr2);
+const arr = ["a", "b", "c"];
+arr[1] = "!";
+console.log(arr);
+arr = [1, 2, 3];
+console.log(arr);
+arr = ["a", "b", "c"];
+console.log(arr);
