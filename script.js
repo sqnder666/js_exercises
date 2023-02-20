90e0dd9e04ac2aa5f467c228e91dc799f91e8623
sqnder666 <i.irudovi21@gmail.com>
2023-02-20 23:17:43 +0300

    78

--- script.js
+++ script.js
@@ -1,20 +1,9 @@
 "use strict";
 
-let num1 = Math.floor(Math.random() * 2);
-let num2 = Math.floor(Math.random() * 2);
-let test1, test2;
-if (num1 === 0) {
-  test1 = "abc";
-} else {
-  test1 = "abcd";
-}
-if (num2 === 0) {
-  test2 = "abc";
-} else {
-  test2 = "abcd";
-}
+let test1 = "123";
+let test2 = 123;
 if (test1 === test2) {
   console.log(`test1 === test2`);
-} else {
-  console.log(`test1 !== test2`);
+} else if (test1 == test2) {
+  console.log(`test1 == test2`);
 }
