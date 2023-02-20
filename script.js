b03993e71e8fe2d9de5833c39e7eda20c719e6e3
sqnder666 <i.irudovi21@gmail.com>
2023-02-20 23:06:22 +0300

    67

--- script.js
+++ script.js
@@ -1,4 +1,13 @@
 "use strict";
 
-console.log(Array.isArray([1, 2, 3]));
-console.log(Array.isArray({ x: 1, y: 2, z: 3 }));
+let test = { x: 1, y: 2, z: 3 };
+console.log(test);
+console.log(test.x);
+let test_1 = [1, 2, 3];
+console.log(test_1);
+console.log(test_1[1]);
+let test2 = 1;
+
+console.log(test_1);
+
+console.log(test_1[test2]);
