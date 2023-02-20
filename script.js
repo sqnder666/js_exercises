8db880689e6e4341152816a34ff50c6b735dce12
sqnder666 <i.irudovi21@gmail.com>
2023-02-20 23:12:58 +0300

    72

--- script.js
+++ script.js
@@ -1,9 +1,3 @@
 "use strict";
 
-let obj = { x: 1, y: 2, z: 3 };
-console.log(obj.x);
-let key = "x";
-console.log(obj[key]);
-let sum = obj.x + obj.y + obj.x;
-console.log(sum);
-console.log(Object.keys(obj).length);
+// пgfgfgfgf
