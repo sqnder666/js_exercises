a348a9ed7131f3329e5c0387f0d2c633b0588d55
sqnder666 <i.irudovi21@gmail.com>
2023-03-01 00:34:23 +0300

    85

--- script.js
+++ script.js
@@ -1,21 +1,7 @@
 "use strict";
 
-let num = 3;
-
-if ((num > 5 && num < 10) || num == 20) {
-  console.log("+++");
+if ( !(num1 >= 0 || num2 <= 10)) {
+	console.log('+++');
 } else {
-  console.log("---");
-}
-
-if (num > 5 || (num > 0 && num < 3)) {
-  console.log("+++");
-} else {
-  console.log("---");
-}
-
-if (num == 9 || (num > 10 && num < 20) || (num > 20 && num < 30)) {
-  console.log("+++");
-} else {
-  console.log("---");
+	console.log('---');
 }
